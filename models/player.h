8d5ae1c1fb057b1c1d395ca8dd6cc89aaa868397
ThomasSedhomBestawors <thomassedhom187@gmail.com>
2024-05-08 04:21:53 +0300

    player

--- models/player.h
+++ models/player.h
@@ -1,32 +1,10 @@
 #ifndef PLAYER_H
 #define PLAYER_H
-#include"string"
-#include <vector>
-#include "footballer.h"
-#include "user.h"
-#include "../Structs/PlayerStruct.cpp"
-#include "../Structs/FootballerStruct.cpp"
-using namespace std;
-class Player :public User {
-private:
-    int PlayerID ;
-    static int idCounter;
-    int budget;
-    string fantasyTeamName;
-    int gameWeekPoints;
-    vector<Footballer> fantasyTeamFootballers;
-public:
-    Player(string fantasyTeamName);
-    ~Player();
-    void setBudget(int budget);
-    int getBudget();
-    int getId();
-    vector<Footballer> getFantasyTeamFootballers();
-    void setGameWeekPoints(int points);
-    int getGameWeekPoints();
-    void setPlayer(Footballer footballer);
-    void updateTeam(Footballer oldFootballerId , Footballer newFootballerId);
 
+class Player
+{
+public:
+    Player();
 };
 
 #endif // PLAYER_H
